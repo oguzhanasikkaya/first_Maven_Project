932e6a908ada7b74570a6ad92f3d956184d2d9ae
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-08-29 22:40:18 +0300

    day05 TestNG ıntro

---
+++ src/test/java/com/cydeo/tests/day05_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.day05_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running");
+
+    }
+
+
+}
