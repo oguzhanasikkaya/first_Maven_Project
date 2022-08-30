00d5686317732862a7421c88027880614264a4db
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-08-30 21:46:35 +0300

    short video testNG ıntro

---
+++ src/test/java/com/cydeo/tests/shortVideo/TestNG/P01_TestNG_Intro.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.shortVideo.TestNG;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.assertEquals;
+
+public class P01_TestNG_Intro {
+
+    @Test(priority = 2)
+    public void testOnePlusFive(){
+        assertEquals(1+5,6);
+    }
+
+    @Test(priority = 1)
+    public void testTenMinusFour(){
+        assertEquals(10-4,6);
+    }
+    @BeforeClass
+    public void setUp(){
+        System.out.println("---------Before class is running--------------");
+    }
+    @AfterClass
+    public void tearDown(){
+        System.out.println("---------After class is running -------------- ");
+    }
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---------Before method is running--------------");
+    }
+    @AfterMethod
+    public  void tearDownMethod(){
+        System.out.println("---------After method is running-----------------");
+    }
+
+}
