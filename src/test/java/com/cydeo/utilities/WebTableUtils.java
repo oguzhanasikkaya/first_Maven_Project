8054eb3145b61c0df2b6ebab850cb6be43fe9f8d
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-09-06 16:59:04 +0300

    WebTableUtils create orderVerify method

--- src/test/java/com/cydeo/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -1,6 +1,8 @@
 package com.cydeo.utilities;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableUtils {
 
@@ -19,4 +21,18 @@ public class WebTableUtils {
         return Driver.getDriver().findElement(By.xpath("//td[.='"+customerName+"']/following-sibling::td[3]")).getText();
     }
 
+    /** • Name: orderVerify ()
+        • Return type: void
+        • Arg1: String costumerName
+        • Arg2: String expectedOrderDate
+     This method should accept above-mentioned arguments and internally assert
+     expectedOrderDate matching actualOrderDate. */
+    public static void orderVerify(String customerName,String expectedOrderData){
+
+        String locator = "//td[.='"+customerName+"']/following-sibling::td[3]";
+        WebElement customerDateCell = Driver.getDriver().findElement(By.xpath(locator));
+        String actualOrderDate = customerDateCell.getText();
+        Assert.assertEquals(actualOrderDate,expectedOrderData);
+
+    }
 }
