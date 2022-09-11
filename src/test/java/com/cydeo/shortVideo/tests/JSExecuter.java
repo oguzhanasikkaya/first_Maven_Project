3f0f3609c87abec8b4e5551f1060cff70a493ea5
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-09-11 17:55:40 +0300

    JSExecuter

---
+++ src/test/java/com/cydeo/shortVideo/tests/JSExecuter.java
@@ -0,0 +1,21 @@
+package com.cydeo.shortVideo.tests;
+
+import com.cydeo.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class JSExecuter extends TestBase {
+    @Test
+    public void scrollingTest() throws InterruptedException {
+        driver.get("https://practice.cydeo.com/");
+
+        WebElement cydeoLink = driver.findElement(By.linkText("CYDEO"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        Thread.sleep(3000);
+        js.executeScript("arguments[0].scrollIntoView(true);"+ "arguments[0].click()",cydeoLink);
+        Thread.sleep(3000);
+    }
+}
